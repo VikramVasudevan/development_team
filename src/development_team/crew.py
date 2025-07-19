12c037c0ab13822c52845e225418aab3df495999
Vikram Vasudevan <vikram.vasudevan@ekahaa.com>
2025-07-19 15:25:18 +0530

    Initial version

---
+++ src/development_team/crew.py
@@ -0,0 +1,50 @@
+from tabnanny import verbose
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+
+
+@CrewBase
+class DevelopmentTeam:
+    """DevelopmentTeam crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    agents_config = "./config/agents.yaml"
+    tasks_config = "./config/tasks.yaml"
+
+    @agent
+    def development_lead(self) -> Agent:
+        return Agent(config=self.agents_config["development_lead"], verbose=True)  # type: ignore[index]
+    
+    @agent
+    def backend_engineer(self) -> Agent:
+        return Agent(config=self.agents_config["backend_engineer"], verbose=True) # type: ignore[index]
+
+    @agent
+    def code_reviewer(self) -> Agent:
+        return Agent(config=self.agents_config["code_reviewer"], verbose=True) # type: ignore[index]
+    
+
+    @task
+    def design_task(self) -> Task:
+        return Task(config=self.tasks_config["design_task"], verbose=True)  # type: ignore[index]
+
+    @task
+    def coding_task(self) -> Task:
+        return Task(config=self.tasks_config["coding_task"], verbose=True) # type: ignore[index]
+
+    @task
+    def code_review_task(self) -> Task:
+        return Task(config=self.tasks_config["code_review_task"], verbose=True) # type: ignore[index]
+
+    @crew
+    def crew(self) -> Crew:
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @agent decorator,
+            process=Process.sequential,
+            verbose=True,
+        )
---
+++ src/development_team/main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from development_team.crew import DevelopmentTeam
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'requirement': 'Create a python program to evaluate the first 10000 terms of the following series and multiple the total by 4:\
+            1 + 1/3 - 1/5 + 1/7 - 1/9 ... \
+            ',
+        'module_name' : 'mod_series_eval'
+    }
+    
+    try:
+        DevelopmentTeam().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        'current_year': str(datetime.now().year)
+    }
+    try:
+        DevelopmentTeam().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        DevelopmentTeam().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        "current_year": str(datetime.now().year)
+    }
+    
+    try:
+        DevelopmentTeam().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
